7c75c7e619b41dc8b449dcce4d493fab8858046b
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-03-25 17:50:15 +0500

    log_complete

--- backend/loggers.py
+++ backend/loggers.py
@@ -7,3 +7,21 @@ rec_data_logger.setLevel(logging.INFO)
 system_logger.setLevel(logging.INFO)
  
 logger_system_fh = logging.FileHandler("backend/logs/system_log.log")
+logger_formatter = logging.Formatter('%(asctime)s - %(message)s')
+
+logger_system_fh.setFormatter(logger_formatter)
+system_logger.addHandler(logger_system_fh)
+
+
+def system_logger_write(message):
+    system_logger.info(message)
+
+
+def data_logger_write(message, number):
+    logger_data_fh = logging.FileHandler("backend/logs/test_data_log{}.log".format(number))
+    logger_data_fh.setFormatter(logger_formatter)
+    rec_data_logger.addHandler(logger_data_fh)
+
+    data = "Test " + str(number) + ": " + message
+    rec_data_logger.info(data)
+    data = ""
