9c70057e8d8bd2053cbd09f236dc80638fa3b8ba
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-03-25 20:55:25 +0500

    MC

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +7,22 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, world!"}
+
+
+@app.get("/create_new_test")
+async def create_new_test():
+    return {"message": "OK"}
+
+
+@app.get("/test_list")
+async def test_list():
+    return {"message": "test_list"}
+
+
+@app.get("/test_detail")
+async def test_list():
+    return {"message": "test_list"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="localhost")
