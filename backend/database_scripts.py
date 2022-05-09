f8c6a5aa0f4681bd969a11f8663b9077357ec775
DmitriyGorbunov1 <dmitrgor1@yandex.ru>
2022-05-09 18:41:10 +0500

    upd

--- backend/database_scripts.py
+++ backend/database_scripts.py
@@ -18,13 +18,8 @@ class DB:
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
-                sql_create_tb_query = """CREATE TABLE IF NOT EXISTS tbl_with_tests (
-                    test_id BIGSERIAL PRIMARY KEY,
-                    duration INTEGER, 
-                    brh_opn INTEGER, 
-                    brh_cls INTEGER, 
-                    before_time INTEGER, 
-                    status BOOLEAN, 
+                sql_create_tb_test_data_query = """CREATE TABLE IF NOT EXISTS tests_data (
+                    test_id BIGSERIAL PRIMARY KEY, 
                     time_after_start INTEGER, 
                     akb_voltage DOUBLE PRECISION, 
                     pressure DOUBLE PRECISION, 
@@ -33,23 +28,40 @@ class DB:
                     valve_temp DOUBLE PRECISION, 
                     valve_current DOUBLE PRECISION, 
                     heating_current DOUBLE PRECISION)"""
-                cursor.execute(sql_create_tb_query)
+
+                cursor.execute(sql_create_tb_test_data_query)
+                
+                sql_create_tb_test_descr_query = """CREATE TABLE IF NOT EXISTS tests_descr (
+                    test_id INTEGER PRIMARY KEY,
+                    duration INTEGER, 
+                    brh_opn INTEGER, 
+                    brh_cls INTEGER, 
+                    before_time INTEGER, 
+                    status BOOLEAN)"""
+                cursor.execute(sql_create_tb_test_descr_query)
         finally:
             self.connection.close()
 
-    
-    def insert_data(self, duration, brh_opn, brh_cls, before_time, status, time_after_start,
-                akb_voltage, press, tank_temp, engine_wall_temp, valve_temp,
+    def insert_descriptin(self):
+        self.DB_connect()
+        try:
+            with self.connection.cursor() as cursor:
+                self.connection.autocommit = True
+
+        finally:
+            self.connection.close()
+
+    def insert_data(self, test_id, time_after_start, akb_voltage, press, tank_temp, engine_wall_temp, valve_temp,
                 valve_current, heating_current):
         self.DB_connect()
         try:
             with self.connection.cursor() as cursor:
                 self.connection.autocommit = True
-                sql_insert_data_query = """INSERT INTO tbl_with_tests (duration, brh_opn, brh_cls, before_time, status, time_after_start,
+                sql_insert_data_query = """INSERT INTO tests_data (test_id, time_after_start,
                 akb_voltage, pressure, tank_temp, engine_wall_temp, valve_temp,
-                valve_current, heating_current) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-                cursor.execute(sql_insert_data_query, (duration, brh_opn, brh_cls, before_time, status,
-                            time_after_start, akb_voltage, press, tank_temp, engine_wall_temp,
+                valve_current, heating_current) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                cursor.execute(sql_insert_data_query, (
+                            test_id, time_after_start, akb_voltage, press, tank_temp, engine_wall_temp,
                             valve_temp, valve_current, heating_current))
         finally:
             self.connection.close()
